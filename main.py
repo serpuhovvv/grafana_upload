3f0bbd64ea52ecc5fb751502af5218341e3ebe2f
serg.pudikov <pudikov_serg77@mail.ru>
2022-11-07 17:28:32 -0500

    final version

--- main.py
+++ main.py
@@ -8,10 +8,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
 from selenium.webdriver.support.wait import WebDriverWait
 import pandas as pd
-import time
 from bs4 import BeautifulSoup
 import requests
 from datetime import date
